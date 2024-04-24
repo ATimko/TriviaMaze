ab5a37b75d739b8c10683dcdb9b83b8b0fe2c908
williamchhiv <williamchhiv9384@gmail.com>
2024-04-23 20:49:46 -0700

    Updated gameState
    (cherry picked from commit f8b9cfb7f68df68cfba1cd1a552d3ea5be704e9a)

--- Maze Trivia/src/gameState.java
+++ Maze Trivia/src/gameState.java
@@ -1,2 +1,20 @@
 public class gameState {
+    void savegame() {
+
+    }
+    void loadGame() {
+
+    }
+    void exitGame() {
+
+    }
+    void aboutMenu() {
+
+    }
+    void gameplayMenu() {
+
+    }
+    void instructionsMenu() {
+
+    }
 }
