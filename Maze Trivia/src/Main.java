3600cffcbf5fb3903130bdd1af3b688e85c42062
Ashley Timko <ashleytimko777@gmail.com>
2024-04-23 21:11:21 -0700

    Updated Main

--- Maze Trivia/src/Main.java
+++ Maze Trivia/src/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    int mazeID = 123; // Change this to your desired maze ID
+    int sizeOfMaze = 10; // Change this to your desired size of maze
+    int entrance = 1; // Change this to your desired entrance (1 for top, 2 for bottom, 3 for left, 4 for right)
 }
