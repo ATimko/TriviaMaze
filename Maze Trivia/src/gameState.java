4390fe16d2ec08f4c609abf75d586fafcea633c4
williamchhiv <williamchhiv9384@gmail.com>
2024-04-23 20:49:20 -0700

    Revert "Updated Door"
    This reverts commit b6b4486144835b02c70f01e2fadd16832fe74824.
    
    (cherry picked from commit 742e8da8e66deaff4df3d624ed4c26a5bc6ec8e2)

--- Maze Trivia/src/gameState.java
+++ Maze Trivia/src/gameState.java
@@ -1,20 +1,2 @@
 public class gameState {
-    void savegame() {
-
-    }
-    void loadGame() {
-
-    }
-    void exitGame() {
-
-    }
-    void aboutMenu() {
-
-    }
-    void gameplayMenu() {
-
-    }
-    void instructionsMenu() {
-
-    }
 }
