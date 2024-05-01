d6e8bbf9760ddde1455c908cc94e0f9ca877f9f1
williamchhiv <williamchhiv9384@gmail.com>
2024-04-30 23:11:57 -0700

    Updated Door

--- Maze Trivia/src/Door.java
+++ Maze Trivia/src/Door.java
@@ -1,11 +1,13 @@
 public class Door {
     boolean lockedDoors(boolean bool){
-        System.out.println("hello");
+        System.out.println("hello there");
+        System.out.println("hello there");
         return true; // COME BACK TO THIS
     }
 
     int doorQuestion(int num) {
-        System.out.println("hello");
+        System.out.println("hello there");
+        System.out.println("hello there");
         return 1; // COME BACK TO THIS
     }
 
