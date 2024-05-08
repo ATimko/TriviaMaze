690ab3c52ff5c418a4ff8d854e5da0a40d37c2d9
Ashley Timko <ashleytimko777@gmail.com>
2024-05-07 19:57:06 -0700

    Updated Questions and added more methods

--- Maze Trivia/src/Questions.java
+++ Maze Trivia/src/Questions.java
@@ -1,2 +1,29 @@
 public class Questions {
+    private String question;
+    private String[] options;
+    private String correctAnswer;
+    private String type;
+
+    public void Question(String question, String[] options, String correctAnswer, String type) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+        this.type = type;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
