ec6bc390e64ee8efb02ba8bf84d08d01b4010fd3
Bao Trinh <Baotrinh081969@gmail.com>
2024-05-14 23:11:17 -0700

    Change to card layout

--- Maze Trivia/src/views/aboutUI.java
+++ Maze Trivia/src/views/aboutUI.java
@@ -1,26 +1,15 @@
 package views;
 
-import Common.mazeConstants;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+import java.awt.*;
 
 public class aboutUI extends JFrame {
 
-    /**
-     * Container to hold body in.
-     */
     private final JPanel myContainer;
 
-    /**
-     * Entry point to create a new window for About.
-     */
     public aboutUI() {
         super("About");
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        this.setSize(mazeConstants.Dimensions.ABOUT_PANEL_DIMENSION);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setVisible(true);
@@ -31,17 +20,13 @@ public class aboutUI extends JFrame {
         createUI();
     }
 
-    /**
-     * Creates the UI.
-     */
     private void createUI() {
         myContainer.setPreferredSize(this.getPreferredSize());
         myContainer.setLayout(new BoxLayout(myContainer, BoxLayout.Y_AXIS));
 
         final JLabel creditsTextLabel = new JLabel();
         creditsTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        creditsTextLabel.setText("");
+        creditsTextLabel.setText("Credits: Developed by [Your Name]");
         myContainer.add(creditsTextLabel);
-
     }
 }
