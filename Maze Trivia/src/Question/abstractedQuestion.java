d8d887f71afae636ce85e29e9df00b204b13bca2
williamchhiv <williamchhiv9384@gmail.com>
2024-05-16 03:04:35 -0700

    Edited questionFactory, added abstractedQuestion

--- Maze Trivia/src/Question/abstractedQuestion.java
+++ Maze Trivia/src/Question/abstractedQuestion.java
@@ -1,6 +1,47 @@
 package Question;
-import java.util.Objects;
-import java.util.Arrays;
-public class abstractedQuestion {
 
+public class abstractedQuestion implements question {
+    private final questionType type;
+    private final String question;
+    private final String[] choices;
+    private final String answer;
+    private final String[] subject;
+
+    protected abstractedQuestion(questionType type, String[] subject, String question, String[] choices, String answer) {
+        if (type == null || subject == null || question == null || choices == null || answer == null) {
+            throw new IllegalArgumentException("You cannot have null Question parameters!");
+        }
+
+        this.type = type;
+        this.question = question;
+        this.choices = choices;
+        this.answer = answer;
+        this.subject = subject;
+    }
+
+    public questionType getType() {
+        return type;
+    }
+
+    public String[] getSubjects() {
+        return subject;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getChoices() {
+        return choices;
+    }
+
+    public boolean correctAnswer(String answer) {
+        return answer.equalsIgnoreCase(this.answer);
+    }
+
+    @Override
+    public boolean equals(Object input) {
+        // write something that checks whether the selected answer matches the correct answer
+        return true;
+    }
 }
--- Maze Trivia/src/Question/questionFactory.java
+++ Maze Trivia/src/Question/questionFactory.java
@@ -1,14 +1,16 @@
 package Question;
 
-public final class questionFactory {
-    /*
-    public static question createQuestion(String question, String[] subject, String[] choices, String answer, String type) {
+public class questionFactory extends abstractedQuestion {
+    public questionFactory(questionType type, String[] subject, String question, String[] choices, String answer) {
+        super(type, subject, question, choices, answer);
+    }
+
+    public static question createQuestion(String question, String[] topics, String[] choices, String answer, String type) {
         return switch (type) {
-            case "MultipleChoice" -> new questionMultipleChoice(question, subject, choices, answer);
-            case "TrueFalse" -> new questionTrueFalse(question, subject, answer);
-            case "ShortAnswer" -> new questionShortAnswer(question, subject, answer);
-            default -> throw new IllegalStateException("Illegal Aug: " + type);
+            case "multipleChoice" -> new abstractedQuestion(questionType.multipleChoice, topics, question, choices, answer) {};
+            case "trueFalse" -> new abstractedQuestion(questionType.trueFalse, topics, question, new String[]{"True", "False"}, answer) {};
+            case "shortAnswer" -> new abstractedQuestion(questionType.shortAnswer, topics, question, new String[0], answer) {};
+            default -> throw new IllegalStateException("Unexpected value: " + type);
         };
     }
-    */
 }
