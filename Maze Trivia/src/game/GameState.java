e0285d9cb4631a6b080bb1e08fbf47a1419f7762
Ashley Timko <ashleytimko777@gmail.com>
2024-06-04 17:18:29 -0700

    The Game State has scoring and maze added onto the file

--- Maze Trivia/src/game/GameState.java
+++ Maze Trivia/src/game/GameState.java
@@ -1,20 +1,40 @@
 package game;
 
+import maze.Maze;
+
 import java.io.Serializable;
 import java.util.List;
 
 public class GameState implements Serializable {
     private static final long serialVersionUID = 1L;
+    private Maze maze;
+    private int score;
     private int playerPositionX;
     private int playerPositionY;
     private int currentRoom;
     private List<String> questionsAnswered;
 
-    public GameState(int playerPositionX, int playerPositionY, int currentRoom, List<String> questionsAnswered) {
+    public GameState(Maze maze, int score, int playerPositionX, int playerPositionY, int currentRoom, List<String> questionsAnswered) {
+        this.maze = maze;
+        this.score = score;
         this.playerPositionX = playerPositionX;
         this.playerPositionY = playerPositionY;
         this.currentRoom = currentRoom;
         this.questionsAnswered = questionsAnswered;
+    }
+    public Maze getMaze() {
+        return maze;
+    }
+
+    public void setMaze(Maze maze) {
+        this.maze = maze;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public int getPlayerPositionX() {
